98e2261a84de8149b12655de88199d9ae66a51eb
Sparsh Johri <bhatabhaiyya@gmail.com>
2020-04-01 19:55:25 -0700

    modified code with ggplot

--- src/Graphing_Data.py
+++ src/Graphing_Data.py
@@ -1,62 +1,74 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Apr  1 15:41:27 2020
+
+@author: bhata
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Fri Mar 27 12:45:54 2020
+
+
+
 
 @author: bhata
 """
 import pandas
 import numpy
-import matplotlib.pyplot as plt
-import scipy
 from plotnine import *
 from plotnine.data import *
-
-
-def rounder(number, place):
-    number = int(number * (10**place))
-    number = number/(10**place)
-    return number
     
 #getting the initial data
 file = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-data = pandas.read_csv(file)
-del data["fips"]
-
+data1 = pandas.read_csv(file)
+del data1["fips"]
 
 #filtering out the counties I don't want to look at
-counties = list(set(data["county"]))
+counties = list(set(data1["county"]))
 county = ["Bergen", "Broward", "Cook", "Dougherty", "Los Angeles",\
-          "Macomb", "Nassau", "New York City", "Oakland", "Orleans", \
-          "Santa Clara", "Snohomish","King"]
-data = data[data["county"].isin(county)]
+          "Macomb", "Nassau", "Oakland", "Orleans", \
+          "Santa Clara", "Snohomish","King", "New York City"]
+data = data1[data1["county"].isin(county)]
 del data["state"]
-data = data.set_index("date")
+minimum_deaths = 1
 
-
-#filtering out the counties I don't want to look at
-
-minimum_deaths = 0
-
+#separating the data by county into a dictionary, then combining back together
+#into a dataframe
 data_by_county = {}
 for i in county:
-    data_by_county[i] = data[data["county"]==i]
-    sub = data_by_county[i]
-    data_by_county[i] = sub[sub["deaths"]>minimum_deaths]
+    county_data = data[data["county"]==i][data[data["county"]==i]["deaths"]>minimum_deaths]
+    county_data.insert(0, "Day", list(range(1, len(county_data)+1)))
+    data_by_county[i] = county_data
+graphable_data = pandas.DataFrame(columns = data_by_county[county[0]].columns)
+for i in data_by_county:
+    graphable_data = graphable_data.append(data_by_county[i], ignore_index = True)
 
-formatted_list_of_data_by_county = []
-for i in county:
-    counter = 0
-    for j in data_by_county[i].index:
-        datapoint = [i, counter, data_by_county[i].loc[j]["deaths"], ]
-        counter += 1
-        formatted_list_of_data_by_county.append(datapoint)
+#transforming all the integer datatypes into numpy integers
+deaths = numpy.asarray(list(graphable_data["deaths"]), dtype=numpy.float64)
+days = numpy.asarray(list(graphable_data["Day"]), dtype=numpy.float64)
+counties = numpy.asarray(list(graphable_data["county"]))
+cases = numpy.asarray(list(graphable_data["cases"]), dtype=numpy.float64)
+graphable_data["deaths"] = deaths
+graphable_data["Day"] = days
+graphable_data["county"] = counties
+graphable_data["cases"] = cases
 
-graphable_data = pandas.DataFrame(formatted_list_of_data_by_county, \
-                                  columns = ["County", "Days", "Deaths"])
+#constants for printing out the graph
+text_size = 20
+graph_w = 15
+graph_h = 8
 
-
-myPlot = ggplot(graphable_data, aes('Days', 'Deaths', color='factor(County)'))\
+#Cases per day
+myPlot = ggplot(graphable_data, aes('Day', 'cases', color="county"))\
  + geom_line(size = 2)\
- + theme(text=element_text(size = 25))
-ggsave(myPlot, width = 15, height = 15)
+ + theme(text=element_text(size = text_size))
+ggsave(myPlot, width = graph_w, height = graph_h)
+myPlot.draw()
+
+#Deaths per day
+myPlot = ggplot(graphable_data, aes('Day', 'deaths', color="county"))\
+ + geom_line(size = 2)\
+ + theme(text=element_text(size = text_size))
+ggsave(myPlot, width = graph_w, height = graph_h)
 myPlot.draw()
